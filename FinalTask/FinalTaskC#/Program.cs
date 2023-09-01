f24f40341745f3cb2aec7e132dcdb324c84603a9
MochalynaG <Har.lya@mail.ru>
2023-09-01 20:27:56 +0300

    написан метод создания нового массива

--- FinalTask/FinalTaskC#/Program.cs
+++ FinalTask/FinalTaskC#/Program.cs
@@ -15,3 +15,17 @@ int GetSizeNewArray(string[] array)
     }
     return count;
 }
+
+string[] FillResultArray(string[] array, int size)
+{
+    string[] result = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - size] = array[i];
+            size--;
+        }
+    }
+    return result;
+}  
