8bca4072bcbc0e11a23d334ec9f49ca766913e37
MochalynaG <Har.lya@mail.ru>
2023-09-18 17:34:03 +0300

    готово

--- PythonHW/PyHomeWork4.py
+++ PythonHW/PyHomeWork4.py
@@ -36,14 +36,14 @@
 # находясь перед некоторым кустом заданной во входном файле грядки.
 
 
-n = int(input("Введите количество кустов (не меньше з): "))
-numbers = list()
-for i in range(n):
-    numbers.append(int(input(f"Введите количество ягод на кусте {i + 1}: ")))
+# n = int(input("Введите количество кустов (не меньше з): "))
+# numbers = list()
+# for i in range(n):
+#     numbers.append(int(input(f"Введите количество ягод на кусте {i + 1}: ")))
 
-numbers_count = list()
-for i in range(len(numbers) - 1):
-    numbers_count.append(numbers[i - 1] + numbers[i] + numbers[i + 1])
-numbers_count.append(numbers[-2] + numbers[-1] + numbers[0])
+# numbers_count = list()
+# for i in range(len(numbers) - 1):
+#     numbers_count.append(numbers[i - 1] + numbers[i] + numbers[i + 1])
+# numbers_count.append(numbers[-2] + numbers[-1] + numbers[0])
 
-print(f"Максимальное число ягод, которое может собрать модуль за один заход : {max(numbers_count)}")
+# print(f"Максимальное число ягод, которое может собрать модуль за один заход : {max(numbers_count)}")
